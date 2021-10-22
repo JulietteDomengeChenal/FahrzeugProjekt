b2ee18d6c5f8520d09b638bfef2dcedf74e23f04
juliette <juliette.domenge-chenal@le-campus-numerique.fr>
2021-10-22 09:24:08 +0200

    first commit

---
+++ Preis/src/main/java/com/example/preis/PreisApplication.java
@@ -0,0 +1,13 @@
+package com.example.preis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreisApplication.class, args);
+    }
+
+}
---
+++ Preis/src/test/java/com/example/preis/PreisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
